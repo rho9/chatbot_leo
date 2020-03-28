950f1ea26dbb69bb2d9a1d9985e3a7c5dbcc84c2
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2020-03-28 18:52:05 +0100

    add introdution file with introductory speech, dictionary that will contains words, synonyms etc, lion_chatbot

---
+++ lion_chatbot.py
@@ -0,0 +1,16 @@
+def main():
+	introduction()
+
+def introduction():
+	intro_file = open('data/introduction.txt', "r")
+	print(intro_file.read())
+	intro_file.close()
+	question = input("\nDo you have any questions?")
+	# not only yes, but synonims
+	print("ok, I answer") if "yes" in question else print("ok, let's go on")
+
+def first_session():
+	return 0
+
+if __name__== "__main__":
+	main()
