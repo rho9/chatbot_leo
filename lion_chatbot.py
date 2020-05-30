03c1adf1c9978e292a2a6551cf07d22eacabbf46
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2020-05-30 12:15:33 +0200

    create session_two.py with comments about what it should do

--- lion_chatbot.py
+++ lion_chatbot.py
@@ -1,20 +1,17 @@
 import session_one as s1
 import strings_manager as sm
-import emotions_manager as em
 FLAG = "fast"
 
 
 def main():
     introduction()
+    s1.s1_manager()
 
 
 def introduction():
     intro_file = open('data/introduction.txt', "r")
     sm.my_print_file(intro_file, FLAG)
     intro_file.close()
-    #s1.s1_manager()
-    sen = "Well, for example, when I’m at work I’m really worried about saying something stupid or sounding weird "
-    sm.complete_keywords_pos(sen, "worried")
 
 
 if __name__ == "__main__":
---
+++ session_two.py
@@ -0,0 +1,11 @@
+# during this session the chatbot should make a recap about what has been said underlying
+# the perception (the number given) that the user has.
+# Then the chatbot should suggest to record a video in which the person act like he/she were
+# in one of the situation he/she talk about with the chatabot.
+# Then the chatbot suggest the user to watch at it focusing on the thing that has ben rated
+# Then the chatbot ask the user to rate again the symptoms
+# Then the chatbot should compare them with the old one:
+# if are rated with a minor number, it points out it to the user and ask him/she what he/she
+# thinks about that
+# if are similar or more? Search in the document, on the internet...?
+
