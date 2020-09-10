984ec08bc886c9e274d58debd519ee627ef52d85
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2020-09-10 22:10:29 +0200

    skip non utf8 characters

--- embedding/glove_cosine_similarity.py
+++ embedding/glove_cosine_similarity.py
@@ -16,35 +16,43 @@ def main():
 def cosine_distance_wordembedding_method(s1, s2):
     model = load_glove_model()
     v1 = []
-    for elem in model:
-        for word in preprocess(s1):
+    word1 = preprocess(s1)
+    word2 = preprocess(s2)
+    print("word1:", word1)
+    print("word2:", word2)
+    for elem in model: # a fare girare tutti gli elementi di model ci metti una vita
+        for word in word1:
             if elem[0] == word:
                 v1.append(elem[1:])
     vector_1 = np.mean(v1, axis=0)
     # vector_1 = np.mean([model.index(word) for word in preprocess(s1)],axis=0)
     v2 = []
     for elem in model:
-        for word in preprocess(s2):
+        for word in word2:
             if elem[0] == word:
                 v2.append(elem[1:])
     vector_2 = np.mean(v2,axis=0)
-    # attenzione: se la parola non è presente si rompe
+    # attenzione: se la parola non è presente si rompe DA VERIFICARE
     # vector_n saranno vettori lunghi il numero di parole della frase presenti nel file glove
     # preprocess immagino che debba mettere le parole in minuscolo, togliere le stop words e la pnteggiatura
     cosine = scipy.spatial.distance.cosine(vector_1, vector_2)
     print('Word Embedding method with a cosine distance asses that our two sentences are similar to',round((1-cosine)*100,2),'%')
 
 
+# fare in modo di utilizzarlo una volta sola perché ci mette un botto
 def load_glove_model():
    # file = open('glove.twitter.27B/glove.twitter.27B.25d.txt', "r", encoding="utf8")
    # file_content = file.read()
    # file.close()
     #return file_content
+   # così non si può fare perché il file è troppo grande (scrivere l'eccezione ottenuta nella tesi?)
     model = []
-    with open("data.txt") as myfile:
+    with open("glove.twitter.27B/glove.twitter.27B.25d.txt", "r", encoding="utf8") as myfile:
         for line in myfile:
-            model.append(line.strip("\n").split(" ")) # attenzione: l'ultimo valore ha anche l'a capo
+            line2 = bytes(line, 'utf-8').decode('utf-8', 'ignore')
+            model.append(line2.strip("\n").split(" ")) # attenzione: l'ultimo valore ha anche l'a capo
     print("Model:", model)
+    # from string to float
     for elem in model:
         i = 1
         while i < len(elem):
