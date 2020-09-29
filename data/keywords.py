9aac2ca90eb5139b7f195f8c795cf4e907637a57
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2020-09-29 19:17:32 +0200

    adjust keywords

--- data/keywords.py
+++ data/keywords.py
@@ -2,58 +2,68 @@ keywords_use = {"concerns": [],
                 "difficult_experience": ["My anxiety is been stopping me a bit from being able to get work",
                                          "Every time there's an opportunity that involves me having to interact with people I just tend to turn it down",
                                          "I just won't talk to anyone I might even stand in a corner and look like the loner at the party",
-                                         "I was kind of making things hard"
+                                         "I was kind of making things hard",
+                                         "In my class each week we have an inclusion activity and generally I guess it's to let people feel like the group's more cohesive and included, but I usually experience it as the opposite",
+                                         "There's usually a task at the beginning of each class in wich you have to take off your shoes and talk about the most interesting place your shoes have been and usually I get really nervous"
                                          ],
-                "event": ["It was a fashion event for a campaign or something but it was really cool but I found really hard focus and I was really anxious",
+                "event": ["It was a fashion event for a campaign and it was really cool but I found really hard to focus and I was really anxious",
                           "I had to work with a lot of different people",
-                          "I'm kind of thinking I'll just get too anxious you know maybe just get to the front door, turn around and I go home"
+                          "I'm kind of thinking I'll just get too anxious, you know.. maybe I'll just get to the front door, turn around and I go home"
                           ],
                 "first_time": ["It was the first time"
                               ],
-                "hard_to_speak": ["One thing that I’ve been noticing lately is that I have a tendency to kind of hang back in groups and I tend to observe instead of participate and I sometimes I get really nervous when I feel like I'm expected to speak up and I want to speak up but I kind of feel like I freeze",
+                "hard_to_speak": ["One thing that I’ve been noticing lately is that I have a tendency to kind of hang back in groups",
+                                  "I tend to observe instead of participate and I sometimes I get really nervous when I feel like I'm expected to speak up",
+                                  "I want to speak up but I kind of feel like I freeze",
                                   "Sometimes when they try to force myself to speak up then I actually don't feel ready and I get like I feel like my throat's closing and I get red-faced",
-                                  "In my class each week we have an inclusion activity and generally I guess it's to let people feel like the group's more cohesive and included, but I usually experience it as the opposite. There's usually a task of the beginning of each class in wich you have to take off your shoe and talk about the most interesting place your shoe has been and stuff like this and usually I get really nervous I might feel like I can't think of anything to say and I feel like I usually end up saying something really awkward or lame"
+                                  "I can't think of anything to say and I feel like I usually end up saying something really awkward or lame"
                                  ],
-                "moved": ["I've just recently moved out for the first time from living with my parents"
+                "moved": ["I've just recently moved out from living with my parents",
+                          "I've just left my parents' home"
                          ],
                 "not_want_happen": ["I don't want that to happen",
                                     "I don't want to lose everyone",
-                                    "I don't want to find myself alone"
-                                    "It's kind of stopping me from making friends"
+                                    "I don't want to find myself alone",
+                                    "It's kind of stopping me from making friends and I don't want this to happen",
+                                    "Nothing of what I'm saying will make sense and people would just walk away and stop talking to me"
                                     ],
-                "safety_behaviours": ["I guess like when I'm holding tight the camera I just grip onto it really really tight and I kind of clench all my muscles and just find something that I can focus on",
-                                      "I just, as I said, look down and avoid eye contact and wear only black cloths so my sweating is less obvious and you just can't tell"
+                "safety_behaviours": ["When I hold tight the camera I just grip onto it really really tight",
+                                      "I kind of clench all my muscles",
+                                      "I just find something that I can focus on",
+                                      "I just look down and avoid eye contact",
+                                      "I wear only black cloths so my sweating is less obvious and you just can't tell"
                                       ],
-                "self_focus": ["stop being lucid", "lose concentration", "can't think",
+                "self_focus": ["I stop being lucid and I can't think",
+                               "I actually tend to lose concentration",
                                "When I'm in the middle of being worried, it's really hard for me to focus",
                                "I can't focus on what I need to do because I feel like my mind is on a lot of different things",
                                "I can't focus on what I should be doing"
                                ],
-                "phys_symp": ["I'm also worried to visibly shake,blushing, and trembling",
-                              "I think obviously the shaking, my hands and legs shaking and kind of going really red and sweating and just looking really weird",
+                "phys_symp": ["I'm also worried to visibly shake, blushing, and trembling",
+                              "I think obviously the shaking, my hands and legs shake and I kind of go really red and sweating and just looking really weird",
                               "I think I just look really obviously weird and out of place and like really red and just visibly shaking and sweating",
                               "Red. Just like a beetroot...Shaky...Nervous...Anxious...",
-                              "I'll just stutter..nothing will make sense of what I'm saying and people would just walk away and stop talking to me",
+                              "I'll just stutter..",
                               "I just get really hot and sweaty"
                               ],
                 "suggest_friend": ["I would tell her she's funny and charming and usually has really cool stories and that she's has lots of good experiences"],
                 "situations": [],
                 "thoughts": ["I just want to go home", "I wish I weren't there",
-                             "can't be articulate", "make peopl laugh", "I'm afraid to sound like an idiot",
+                             "I think I can't be articulate", "I'll just make people laugh",
+                             "I'm afraid to sound like an idiot",
                              "Having a need to say like don't be boring or you're not making any sense",
                              "They think I'm really stupid", "They're all going to be staring",
                              "People think I just can't do my job", "They'll think that I just look like a weirdo",
-                             "I'm just an idiot"
+                             "My colleagues will think I'm just an idiot"
                              ],
-                "work_problem": ["I'm just getting less and less work as it goes onand I'm afraid of being fired",
+                "work_problem": ["I'm just getting less and less work as it goes on and I'm afraid of being fired",
                                  "When I'm at work I'm worried a lot about how I come across in social situations",
                                  "I don't like when I have to work with other people because it makes me anxious"
                                  ],
                 "worried": ["I'm really worried to say something stupid", "I'm worried to look like an idiot",
-                            "I'm worried to lose my job", "I'm worried"
+                            "I'm concerned to lose my job", "I'm worried"
                             ]
                 }
-
 
 
 keywords_count = {"concerns": [],
