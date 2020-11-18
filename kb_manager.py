6e2fc6214927a2f0840a36cce762e6ee484461c6
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2020-11-18 16:11:38 +0100

    change comments

--- kb_manager.py
+++ kb_manager.py
@@ -6,13 +6,13 @@ from data import keywords as kw
 FLAG = "fast"
 
 
-# non dovrebbe servire più perché ora si interroga la grammatica invece di questo
 # given a key, it returns randomly one of its value
 def find_value(key):
     values = kb.dictionary[key]
     return values[random.randint(0, (len(values)-1))]
 
 
+# given a value, it returns its key
 def get_key(val):
     for key, values in kw.keywords_use.items():
         for value in values:
@@ -21,7 +21,7 @@ def get_key(val):
     return "key doesn't exist"
 
 
-# given a sentence, it finds the keys present in it
+# given a sentence, it finds the keywords in it
 def find_keywords(sentence):
     keywords_list = []
     for keyword in kb.keywords:
@@ -30,17 +30,18 @@ def find_keywords(sentence):
     return keywords_list
 
 
-# given a sentence, it finds the rate present in it
+# given a sentence, it finds the rate in it
 def find_rate(sentence):
     final_rate = None
     for rate in kb.rates:
         if rate in sentence.lower():
             final_rate = rate
-            break  # in this way if the user writes 9 out of 10, I save 9
+            break  # if the user writes 9 out of 10, LEO saves 9
     return final_rate
 
 
-def ask_for_rate(reaction):  # salvare l'intero così da poter fare il confronto?
+# it asks the user to give a rate and it saves it
+def ask_for_rate(reaction):
     question = cl.choose_sentence("rating")
     if "*" in question:
         question = sm.replace_a_star(question, reaction)
@@ -65,13 +66,7 @@ def find_typology(keyword):
     return typology
 
 
-# work in progess for session 2
-def update_db(situations, old_thought, new_thought, new_rate):
-    # THOUGHTS
-    situations[0].change_thought(old_thought, new_thought, new_rate)
-
-
-# print what has been saved from user's answers
+# it prints what has been saved from user's answers
 def print_db(concerns, situations):
     print("Concern: ", concerns[0].get_concern())
     print("Situation: ", situations[0].get_situation())
